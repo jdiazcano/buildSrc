634b4da8d152e6c027ed6a6b76407ded0a110837
jdiazcano <terkitos@gmail.com>
2020-04-14 23:17:11 +0100

    Added main class for fatjar.gradle.kts

--- gradle/fatjar.gradle.kts
+++ gradle/fatjar.gradle.kts
@@ -1,3 +1,8 @@
 tasks.register<Jar>("fatjar") {
+    manifest {
+        attributes["Main-class"] = project.extra["mainClass"]
+    }
+
     from(Callable { configurations["runtimeClasspath"].map { if (it.isDirectory) it else zipTree(it) } })
+    with(tasks["jar"] as CopySpec)
 }
